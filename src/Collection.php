76390c1bb8f49a0c4ab4aecda4e0b68f905fc43f
Sergiy Tsybulko <sergiy@tsybulko.com>
2017-07-05 08:11:56 +0000

    add comments to Collection class

--- src/Collection.php
+++ src/Collection.php
@@ -2,20 +2,37 @@
 
 namespace Translator;
 
+/**
+ * Class Collection
+ * @package Translator
+ */
 class Collection implements \IteratorAggregate
 {
+    /**
+     * @var array $collections
+     */
     public $collections = [];
 
+    /**
+     * Collection constructor.
+     * @param array $collections
+     */
     public function __construct(array $collections = [])
     {
         $this->collections = $collections;
     }
 
+    /**
+     * @param $collection
+     */
     public function add($collection)
     {
         $this->collections[] = $collection;
     }
 
+    /**
+     * @return \ArrayIterator
+     */
     public function getIterator()
     {
         return new \ArrayIterator($this->collections);
