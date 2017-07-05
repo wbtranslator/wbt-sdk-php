b89d71886759ea3349564638fe42363832ad62bb
Sergiy Tsybulko <sergiy@tsybulko.com>
2017-07-05 07:36:39 +0000

    fixed construct base exception class

--- src/Exceptions/TranslatorException.php
+++ src/Exceptions/TranslatorException.php
@@ -10,9 +10,16 @@ use Throwable;
  */
 class TranslatorException extends \Exception
 {
+    /**
+     * TranslatorException constructor.
+     * @param string|array $message
+     * @param int $code
+     * @param Throwable|null $previous
+     */
     public function __construct($message = "", $code = 0, Throwable $previous = null)
     {
-        $message = is_array($message) ? $message[key($message)][0] : $message;
+        $message = is_array($message) ? !empty($message[key($message)][0])
+            ? $message[key($message)][0] : '' : $message;
 
         parent::__construct($message, $code, $previous);
     }
