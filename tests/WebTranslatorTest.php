1d7e6f53e31500a0c50fd11d5117160d57a8aa8d
Sergiy Tsybulko <sergiy@tsybulko.com>
2017-07-10 13:12:57 +0000

    implemented WebTranslatorTest

--- tests/WebTranslatorTest.php
+++ tests/WebTranslatorTest.php
@@ -5,10 +5,69 @@ namespace WebTranslator\Tests;
 use PHPUnit\Framework\TestCase;
 use WebTranslator\WebTranslator;
 
+/**
+ * Class WebTranslatorTest
+ *
+ * @package WebTranslator\Tests
+ */
 class WebTranslatorTest extends TestCase
 {
-    public function testEmpty()
+    protected $apiKey = 'test_api_key';
+
+    /**
+     * @return WebTranslator
+     */
+    public function testWebTranslator()
     {
-        $this->assertTrue(true);
+        $translator = new WebTranslator($this->apiKey);
+
+        $this->assertEquals($this->apiKey, $translator->getApiKey());
+
+        return $translator;
+    }
+
+    /**
+     * @depends testWebTranslator
+     * @param WebTranslator $translator
+     */
+    public function testGetClient(WebTranslator $translator)
+    {
+        $this->assertInstanceOf(\GuzzleHttp\ClientInterface::class, $translator->getClient());
+    }
+
+    /**
+     * @depends testWebTranslator
+     * @param WebTranslator $translator
+     */
+    public function testRequest(WebTranslator $translator)
+    {
+        $this->assertInstanceOf(\WebTranslator\Interfaces\RequestInterface::class, $translator->request());
+    }
+
+    /**
+     * @depends testWebTranslator
+     * @param WebTranslator $translator
+     */
+    public function testResource(WebTranslator $translator)
+    {
+        $this->assertInstanceOf(\WebTranslator\Interfaces\ResourceInterface::class, $translator->resource());
+    }
+
+    /**
+     * @depends testWebTranslator
+     * @param WebTranslator $translator
+     */
+    public function testTranslations(WebTranslator $translator)
+    {
+        $this->assertInstanceOf(\WebTranslator\Interfaces\ResourceInterface::class, $translator->translations());
+    }
+
+    /**
+     * @depends testWebTranslator
+     * @param WebTranslator $translator
+     */
+    public function testGroups(WebTranslator $translator)
+    {
+        $this->assertInstanceOf(\WebTranslator\Interfaces\ResourceInterface::class, $translator->groups());
     }
 }
