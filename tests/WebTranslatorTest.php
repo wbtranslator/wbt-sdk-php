a48efcb723fe1692f26bf9a4b6369b9562caced1
Sergiy Tsybulko <sergiy@tsybulko.com>
2017-07-10 12:31:38 +0000

    init tests

---
+++ tests/WebTranslatorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace WebTranslator\Tests;
+
+use PHPUnit\Framework\TestCase;
+use WebTranslator\WebTranslator;
+
+class WebTranslatorTest extends TestCase
+{
+    public function testEmpty()
+    {
+        $this->assertTrue(true);
+    }
+}
