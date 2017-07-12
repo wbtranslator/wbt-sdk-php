6cd152a1efa7427e9545eb71986775e7912769bb
Sergiy Tsybulko <sergiy@tsybulko.com>
2017-07-12 15:11:42 +0000

    fixed config file

--- examples/config.php
+++ examples/config.php
@@ -5,9 +5,10 @@ use WebTranslator\WebTranslator;
 require_once dirname(__FILE__) . '/../vendor/autoload.php';
 
 define('TRANSLATOR_API_KEY', '4fd8f2d2cf84c3c1eb54cfd499b2f441');
+$client = null;
 
-$client = new \GuzzleHttp\Client([
-    'base_uri' => 'http://192.168.88.149:8080/api/project/'
-]);
+/*$client = new \GuzzleHttp\Client([
+    'base_uri' => 'http://localhost/api/project/'
+]);*/
 
 $translator = new WebTranslator(TRANSLATOR_API_KEY, $client);
